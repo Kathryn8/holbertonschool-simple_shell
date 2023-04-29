267f6c9bb4fab13cf48ebc9e72c92b7c246ec56e
Kathryn6368 <6368@holbertonstudents.com>
2023-04-29 03:17:27 -0700

    add helper functions

--- get_path.c
+++ get_path.c
@@ -5,7 +5,9 @@
 #include <errno.h>
 
 void split_string_into_words(char* string, char** words);
-
+char *get_env_string(char *env_key);
+DIR *_opendir(char *name);
+struct dirent *_readdir(DIR *dp);
 /**
  * get_path - find the path of a file if it exist in PATH variable
  * @name: a string of the name of program to search for
@@ -18,47 +20,19 @@ char *get_path(char **name, char *program_name)
 	char *string;
 	char *paths;
 	char *each_path[100];
-	size_t n;
-	char *env_key;
 	DIR *dp;
 	struct dirent *ep;
 	extern char **environ;
 	char *return_string;
 
-	env_key = "PATH";
-	n = strlen(env_key);
-	i = 0;
-	if (environ == NULL)
-	{
-		return (NULL);
-	}
-	while (environ[i] != NULL)
-	{
-		if (strncmp(environ[i], env_key, n) == 0)
-		{
-			string = strchr(environ[i], '=');
-			paths = strdup(string + 1);
-			if (paths == NULL)
-			{
-				return (NULL);
-			}
-			split_string_into_words(paths, each_path);
-		}
-		i = i + 1;
-	}
+	string = get_env_string("PATH");
+	paths = strdup(string + 1);
+	split_string_into_words(paths, each_path);
 	i = 0;
 	while (each_path[i] != NULL)
 	{
-		dp = opendir(each_path[i]);
-		if (dp == NULL)
-		{
-			perror ("Couldn't open the directory");
-		}
-		ep = readdir(dp);
-		if (ep == NULL)
-		{
-			printf("Error: Could not read directory\n");
-		}
+		dp = _opendir(each_path[i]);
+		ep = _readdir(dp);
 		while (ep != NULL)
 		{
 			if (strcmp(*name, ep->d_name) == 0)
@@ -66,7 +40,7 @@ char *get_path(char **name, char *program_name)
 				return_string = calloc(sizeof(*return_string) * (strlen(*name) + 2 + strlen(each_path[i])), 1);
 				if (return_string == NULL)
 				{
-					printf("Error: Malloc failed\n");
+					perror("malloc");
 					return (NULL);
 				}
 				strcat(return_string, each_path[i]);
@@ -78,7 +52,7 @@ char *get_path(char **name, char *program_name)
 				closedir(dp);
 				return (*name);
 			}
-			ep = readdir(dp);
+			ep = _readdir(dp);
 		}
 		closedir (dp);
 		i = i + 1;
@@ -88,6 +62,13 @@ char *get_path(char **name, char *program_name)
 	return (NULL);
 }
 
+/**
+ * split_string_into_words - use strtok to create an array of strings from a string
+ * @string: pointer to a string
+ * @words: an array of strings or tokens returned from strtok
+ *
+ * Return: void
+ */
 void split_string_into_words(char* string, char** words)
 {
 	char *word;
@@ -102,4 +83,69 @@ void split_string_into_words(char* string, char** words)
 		word = strtok(NULL, ":");
 	}
 	words[i] = NULL;
+}
+
+/**
+ * split_path_into_each_path - return pointer the paths string
+ * @env_key: a string to search against environment variables
+ * @each_path: pointer to array of strings
+ * @path_string: double pointer to a string
+ *
+ * Return: void
+ */
+char *get_env_string(char *env_key)
+{
+	int i, n;
+	char *string;
+	extern char **environ;
+
+	n = strlen(env_key);
+	i = 0;
+	while (environ[i] != NULL)
+	{
+		if (strncmp(environ[i], env_key, n) == 0)
+		{
+			string = strchr(environ[i], '=');
+			return (string);
+		}
+		i = i + 1;
+	}
+	return (NULL);
+}
+
+/**
+ * _opendir - custom opendir function
+ * @name - directory name
+ *
+ * Return: pointer to directory stream
+ */
+DIR *_opendir(char *name)
+{
+		DIR *dp;
+
+		dp = opendir(name);
+		if (dp == NULL)
+		{
+			perror("open directory");
+			return (NULL);
+		}
+		return (dp);
+}
+
+/**
+ * _opendir - custom opendir function
+ * @name - directory name
+ *
+ * Return: pointer to directory stream
+ */
+struct dirent *_readdir(DIR *dp)
+{
+	struct dirent *ep;
+
+	ep = readdir(dp);
+	if (ep == NULL)
+	{
+		return (NULL);
+	}
+	return (ep);
 }
